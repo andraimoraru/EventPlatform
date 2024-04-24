2b2508d3556193b47196e00bbbd1d46b9e99ed43
andraimoraru <andraimoraru>
2024-04-24 11:40:06 +0100

    CSS updates

--- frontend/src/Components/EventDisplay/EventDisplay.jsx
+++ frontend/src/Components/EventDisplay/EventDisplay.jsx
@@ -45,16 +45,16 @@ export const EventDisplay = (event) => {
     <div className="">
                 <div className="eventdisplay">
                 <div className='eventdisplay-left'><img className='image' src={event.image} alt="" /></div>
-                    <div className="eventdisplay-right">
-                        <div className="event_title"><h1>{event.title}</h1></div>        
-                        <div className="event_details">
-                            <div className="eventlocation">{event.location}</div>
-                            <div className="eventprice"> £{event.price}</div>
-                        </div>
-                        <div className="event_details">
+                <div className="eventdisplay-right">
+                    <div className="event_title"><h1>{event.title}</h1></div>        
+                    <div className="event_details">
+                        <div className="eventlocation">{event.location}</div>
+                        <div className="eventprice"> £{event.price}</div>
+                    </div>
+                    <div className="event_details">
                             <div className="eventstart">{new Date(event.date_start).toUTCString()}</div>
                             <div className="eventend"> {new Date(event.date_end).toUTCString()}</div>
-                        </div>
+                    </div>
                         {!isBooked ?    (
                                             <button onClick={() => book_event(event.id)}>Book your place now</button>
                                         ) : (
